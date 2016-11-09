7799e993d9f30560a6b5933b79992404ecaaec5f
Tomasz Bieruta <tomasz.bieruta.ext@nokia.com>
2016-11-09 16:03:52 +0100

    Repaired run-21-con[...] file and made changes in CMakeLists.txt

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,29 @@
 #pragma once
-
+#include <cstddef>
+#include <utility>
 template<typename T>
-class container_wrapper;
+
+ class container_wrapper{
+
+public: container_wrapper() = default;
+
+container_wrapper(T arg) : _value(arg) {
+}
+container_wrapper(const container_wrapper &) = default;
+container_wrapper(container_wrapper &&) = default;
+container_wrapper & operator = (const container_wrapper &) = default;
+container_wrapper & operator =(container_wrapper &&) = default;
+
+
+
+ std::size_t size() const {
+
+	return _value.size();
+}
+
+private: T _value;
+};
+
+
+
 
