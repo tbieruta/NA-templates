e4d1e778400d47ec34ac4648de3a667c46d2f1b6
Tomasz Bieruta <tomasz.bieruta.ext@nokia.com>
2016-11-10 16:01:59 +0100

    refactoring 40 tbieruta

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -3,6 +3,22 @@
 namespace training
 {
 template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred) {
+
+	while (begin !=end) {
+
+		if(pred(*begin)) {
+			 return begin;
+
+		}
+
+		++begin;
+
+	}
+
+	return end;
 }
+
+}
+
 
--- 40-algos-impl/inner_product.h
+++ 40-algos-impl/inner_product.h
@@ -5,6 +5,15 @@ namespace training
 template<typename InputIt1, typename InputIt2, typename T,
     typename BinaryOperation1, typename BinaryOperation2>
 T inner_product(InputIt1 first1, InputIt1 last1, InputIt2 first2, T value,
-    BinaryOperation1 op1, BinaryOperation2 op2);
-}
+    BinaryOperation1 op1, BinaryOperation2 op2) {
 
+	while(first1 != last1) {
+
+		value = op1(value, op2(*first1,*first2));
+		++first1;
+		++first2;
+	}
+
+	return value;
+}
+}
