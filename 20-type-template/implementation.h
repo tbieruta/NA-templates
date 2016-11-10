acdbbe8172450126a4bbffea26cd423566bcb45a
Tomasz Bieruta <tomasz.bieruta.ext@nokia.com>
2016-11-10 11:54:54 +0100

    tbieruta's modification 22, 23, imple

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,28 +1,73 @@
 #pragma once
 #include <cstddef>
 #include <utility>
+#include <boost/optional.hpp>
 template<typename T>
+class container_wrapper{
 
- class container_wrapper{
+public:
+	container_wrapper() = default;
 
-public: container_wrapper() = default;
+	container_wrapper(T arg) : _value(arg) {
 
-container_wrapper(T arg) : _value(arg) {
-}
-container_wrapper(const container_wrapper &) = default;
-container_wrapper(container_wrapper &&) = default;
-container_wrapper & operator = (const container_wrapper &) = default;
-container_wrapper & operator =(container_wrapper &&) = default;
+	}
+
+	container_wrapper(const container_wrapper &) = default;
+
+	container_wrapper(container_wrapper &&) = default;
+
+	container_wrapper & operator = (const container_wrapper &) = default;
+
+	container_wrapper & operator =(container_wrapper &&) = default;
 
 
 
- std::size_t size() const {
+	std::size_t size() const {
 
-	return _value.size();
-}
+		return _value.size();
 
-private: T _value;
+	}
+
+private:
+ 
+	T _value;
+
+
 };
+
+template<typename T>
+class container_wrapper<boost::optional<T>> {
+
+public:
+        container_wrapper() = default;
+
+        container_wrapper(boost::optional<T> arg) : _value(std::move(arg)) {
+
+        }
+
+        container_wrapper(const container_wrapper &) = default;
+
+        container_wrapper(container_wrapper &&) = default;
+
+        container_wrapper & operator = (const container_wrapper &) = default;
+
+        container_wrapper & operator =(container_wrapper &&) = default;
+
+
+
+        std::size_t size() const {
+
+                return _value? 1 : 0;
+
+        }
+
+private:
+ 
+        boost::optional<T> _value;
+
+
+};
+
 
 
 
