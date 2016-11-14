fc23ba60c21a921684f2416fa0128a46847665d4
Tomasz Bieruta <tomasz.bieruta.ext@nokia.com>
2016-11-14 11:55:22 +0100

    50 refactoring tbieruta

--- 50-sfinae/implementation.h
+++ 50-sfinae/implementation.h
@@ -1,8 +1,35 @@
 #pragma once
+#include <type_traits>
 
 template<typename T>
-struct has_type_typedef;
+struct has_type_typedef{
+
+
+	
+private:
+	template<typename U>
+	static auto test(int) ->decltype(std::declval<typename U::type>(),void());
+
+	template<typename U>
+	static char test(long);
+
+public:	
+	static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+
+};
 
 template<typename T>
-struct has_member_variable;
+struct has_member_variable{
+	
+private:
+	template<typename U>
+	static auto test(int) -> decltype(std::declval<U>().member,void());
+
+	template<typename U>
+	static char test(long);
+
+public:
+	static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+
+};
 
